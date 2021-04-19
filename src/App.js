88bebf62464cb8e524760adf319b0e409707d827
kaguya-ma <lulamae1983@gmail.com>
2021-04-19 20:43:21 +0100

    Add style, gradient and formatting

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div>
         <h1 id={'title'}>Random Color Picker</h1>
-        <p id={'p'}>Your Color</p>
+        <p id={'firstColor'}>RGB Value of First Color</p>
+        <p id={'secondColor'}>RGB Value of Second Color</p>
     </div>
   );
 
--- src/Button.js
+++ src/Button.js
@@ -6,17 +6,24 @@ function Button() {
         let green = Math.floor(Math.random() * 255) + 1;
         let blue = Math.floor(Math.random() * 255) + 1;
         let color = `rgb(${red}, ${green}, ${blue})`
+        let red2 = Math.floor(Math.random() * 255) + 1;
+        let green2 = Math.floor(Math.random() * 255) + 1;
+        let blue2 = Math.floor(Math.random() * 255) + 1;
+        let color2 = `rgb(${red2}, ${green2}, ${blue2})`
+        let degrees = Math.floor(Math.random() * 360) + 1;
 
-
-        document.body.style.backgroundColor=color;
-        document.querySelector('#p').innerHTML=color;
+        document.body.style.background=`linear-gradient(${degrees}deg, ${color} 40%, ${color2} 100%)`
+        document.querySelector('#firstColor').innerHTML=color;
+        document.querySelector('#secondColor').innerHTML=color2;
         if ((red < 100) || (green < 100) || (blue < 100)) {
-            document.querySelector('#p').style.color='white'
+            document.querySelector('#firstColor').style.color='white'
+            document.querySelector('#secondColor').style.color='white'
             document.querySelector('#title').style.color='white'
         }
 
         else {
-            document.querySelector('#p').style.color='black'
+            document.querySelector('#firstColor').style.color='black'
+            document.querySelector('#secondColor').style.color='black'
             document.querySelector('#title').style.color='black'
         }
     }
